07b2f874df21a28724895c147846a001cf1fafd5
QuocDat259 <cunktvn06@gmail.com>
2023-05-24 03:24:07 +0700

    DatSan

--- WebsiteDatSan/Controllers/DatSanController.cs
+++ WebsiteDatSan/Controllers/DatSanController.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteDatSan.Models;
 
 namespace WebsiteDatSan.Controllers
 {
     public class DatSanController : Controller
     {
+        WebCauLongss db = new WebCauLongss();
+
         // GET: DatSan
-        public ActionResult Index()
+        public ActionResult DatSan(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            GioDat gioDat = db.GioDat.Find(id);
+            if (gioDat == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Lấy thông tin người đăng nhập hiện tại
+            var userId = User.Identity.GetUserId();
+
+            // Lấy thông tin từ bảng San
+            var san = db.San.FirstOrDefault(s => s.MaSan == gioDat.San.MaSan);
+
+            // Truyền thông tin người đăng nhập, GioDat và San cho view
+            ViewBag.UserId = userId;
+            ViewBag.GioDat = gioDat;
+            ViewBag.San = san;
+
+            return View(gioDat);
         }
     }
 }
