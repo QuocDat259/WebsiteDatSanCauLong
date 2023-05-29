50c2027cdc5814db0566d720c42b438ccd35e45f
QuocDat259 <cunktvn06@gmail.com>
2023-05-29 15:36:23 +0700

    Admin QLHoaDon

---
+++ WebsiteDatSan/Areas/Admin/Controllers/QLHoaDonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteDatSan.Models;
+
+namespace WebsiteDatSan.Areas.Admin.Controllers
+{
+    public class QLHoaDonController : Controller
+    {
+        // GET: Admin/QLHoaDon
+        private WebCauLongss db = new WebCauLongss();
+
+        // GET: HoaDon
+        public ActionResult HoaDon()
+        {
+            // Lấy danh sách tất cả hóa đơn
+            IEnumerable<HoaDon> hoaDonList = db.HoaDon.ToList();
+            ViewBag.UserName = User.Identity.GetUserName();
+            return View("HoaDon", hoaDonList);
+        }
+    }
+}
---
+++ WebsiteDatSan/Areas/Admin/Controllers/QLTatCaHoaDonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteDatSan.Models;
+
+namespace WebsiteDatSan.Areas.Admin.Controllers
+{
+    public class QLTatCaHoaDonController : Controller
+    {
+        private WebCauLongss db = new WebCauLongss();
+
+        // GET: Admin/QLTatCaHoaDon/HoaDon
+        public ActionResult HoaDon()
+        {
+            IEnumerable<HoaDon> hoaDonList = db.HoaDon.ToList();
+            ViewBag.UserName = User.Identity.GetUserName();
+            return View(hoaDonList);
+        }
+    }
+}
