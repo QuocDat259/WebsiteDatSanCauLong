8cf568bd9a38f05759204f8f427166357bf156de
QuocDat259 <cunktvn06@gmail.com>
2023-05-29 17:56:33 +0700

    toi29

---
+++ WebsiteDatSan/Models/AdminVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteDatSan.Models
+{
+    public class AdminVM
+    {
+        public HoaDon HoaDon { get; set; }
+        public AspNetUsers AspNetUser { get; set; }
+        public List<AspNetUsers> AspNetUsers { get; set; }
+        public List<HoaDon> HoaDons { get; set; }
+        public List<San> Sans { get; set; }
+    }
+}
